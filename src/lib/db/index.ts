0e9d838e7f14e4012be70fae9919b026354a8fa6
toono <52028799+toono-f@users.noreply.github.com>
2024-12-29 11:51:21 +0900

    refactor

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,5 @@
-import { drizzle } from "drizzle-orm/neon-serverless";
-import { Pool } from "@neondatabase/serverless";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-export const db = drizzle(pool);
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
